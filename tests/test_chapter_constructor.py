669bd343522bc2bcc26dc755903616ab279f6918
Chuburova Alina <amminami@yandex.ru>
2024-04-07 19:19:12 +0300

    add test_open_bread_section in file test_chapter_constructor.py

--- tests/test_chapter_constructor.py
+++ tests/test_chapter_constructor.py
@@ -8,3 +8,13 @@ from conftest import page
 import settings
 
 class TestConstructorStellarBurgers:
+
+    def test_open_bread_section(self, driver, page):
+        # проверяем в конструкторе, что переход работает в раздел "Булки"
+        driver.get(settings.URL)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.MAIN_PAGE_LOGIN_BUTTON))
+        # выбрала сначал другую категорию "Начинки", а затем проверила уже "Булки"
+        driver.find_element(*page.MAIN_PAGE_FILLINGS_TAB).click()
+        assert driver.find_element(*page.MAIN_PAGE_TAB_CONSTRUCTOR).text == "Начинки"
+        driver.find_element(*page.MAIN_PAGE_BREAD_TAB).click()
+        assert driver.find_element(*page.MAIN_PAGE_TAB_CONSTRUCTOR).text == "Булки"
