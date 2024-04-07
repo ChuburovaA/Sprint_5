33b93a45f46f15aa0bad873b34af271a99eeca8e
Chuburova Alina <amminami@yandex.ru>
2024-04-07 19:12:01 +0300

    add test_check_transfer_from_account_page_to_constructor in test_profile_page.py

--- tests/test_profile_page.py
+++ tests/test_profile_page.py
@@ -19,3 +19,20 @@ class TestProfilePageStellarBurgers:
         driver.find_element(*page.AUTH_PAGE_LOGIN_BUTTON)
 
         assert driver.find_element(*page.HEADER_LOGIN)
+
+    def test_check_transfer_from_account_page_to_constructor(self, driver, page):
+        # проверяем переход из личного кабинета по клику в "Конструктор"
+        # зашли в свой профиль
+        driver.get(settings.URL + "/login")
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.HEADER_LOGIN))
+        driver.find_element(*page.AUTH_PAGE_LOGIN_FIELD).send_keys(StellarBurgersServiceTestData.AUTH_EMAIL)
+        driver.find_element(*page.AUTH_PAGE_PASSWORD_FIELD).send_keys(StellarBurgersServiceTestData.AUTH_PASSWORD)
+        driver.find_element(*page.AUTH_PAGE_LOGIN_BUTTON).click()
+        # нажали на кнопку "Личный кабинет" на главной странице
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.MAIN_PAGE_PROFILE_LINK))
+        driver.find_element(*page.MAIN_PAGE_PROFILE_LINK).click()
+        # нажали на "Конструктор"
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.PROFILE_PAGE_CONSTRUCTOR_LINK))
+        driver.find_element(*page.PROFILE_PAGE_CONSTRUCTOR_LINK).click()
+
+        assert driver.find_element(*page.PROFILE_PAGE_CONSTRUCTOR_LINK)
