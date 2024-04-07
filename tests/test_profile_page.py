bd0e75aa35ae907e5e1869eddc963e5a51f7eefb
Chuburova Alina <amminami@yandex.ru>
2024-04-07 19:11:08 +0300

    add test_check_open_profile_from_main_page in test_profile_page.py

--- tests/test_profile_page.py
+++ tests/test_profile_page.py
@@ -8,3 +8,14 @@ from conftest import page
 import settings
 
 class TestProfilePageStellarBurgers:
+
+    def test_check_open_profile_from_main_page(self, driver, page):
+        # проверяем переход по клику на "Личный кабинет" со стартовой страницы
+        driver.get(settings.URL)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.MAIN_PAGE_LOGIN_BUTTON))
+        driver.find_element(*page.MAIN_PAGE_LOGIN_BUTTON).click()
+
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.AUTH_PAGE_LOGIN_BUTTON))
+        driver.find_element(*page.AUTH_PAGE_LOGIN_BUTTON)
+
+        assert driver.find_element(*page.HEADER_LOGIN)
