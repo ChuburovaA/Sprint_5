743f901720fa3105a4cdc954aaa46781b5832fca
Chuburova Alina <amminami@yandex.ru>
2024-04-07 19:00:11 +0300

    add class TestStellarBurgersRegistration in file test_registration.py

---
+++ tests/test_registration.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from data import StellarBurgersServiceTestData
+from random_emails import generate_random_emails
+from conftest import driver
+from conftest import page
+import settings
+
+
+class TestStellarBurgersRegistration:
