8eb96690434d5d86c18f4f1da6874b55969e4ad8
Chuburova Alina <amminami@yandex.ru>
2024-04-07 18:51:04 +0300

    add locator - button (log in account and place an order) in file locators.py

--- locators.py
+++ locators.py
@@ -8,3 +8,9 @@ class StellarBurgersLocators:
     REG_BUTTON = (By.XPATH, ".//button[text()='Зарегистрироваться']")  # кнопка "Зарегистрироваться"
     REG_PAGE_LOGIN_LINK = (By.XPATH, ".//a[text()='Войти']")  # кнопка "Войти"
     REG_PAGE_ERROR_MESSAGE = (By.CLASS_NAME, "input__error")
+
+    # Кнопка "Войти в аккаунт"/"Оформить заказ" на Главной странице
+    MAIN_PAGE_LOGIN_BUTTON = (
+    By.XPATH, ".//button[text()='Войти в аккаунт']")  # Кнопка "Войти в аккаунт" на главной странице
+    MAIN_PAGE_ORDER_BUTTON = (
+    By.XPATH, ".//button[text()='Оформить заказ']")  # Кнопка "Оформить заказ" на главной странице
