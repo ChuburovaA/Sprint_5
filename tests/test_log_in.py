f1ce9c4a3ca7bc67df863036d9f2e2eff98ddb54
Chuburova Alina <amminami@yandex.ru>
2024-04-09 16:13:59 +0300

    replacment in all file with settings.py for URLS (import)

--- tests/test_log_in.py
+++ tests/test_log_in.py
@@ -11,7 +11,7 @@ import URLS
 class TestLogInStellarBurgers:
     def test_check_button_login_in_main_page(self, driver, page):
         # Нажатие на кнопку "Войти в аккаунт" на главной странице
-        driver.get(settings.URL)
+        driver.get(URLS.URL)
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.MAIN_PAGE_LOGIN_BUTTON))
         driver.find_element(*page.MAIN_PAGE_LOGIN_BUTTON).click()
         # теперь авторизируемся
@@ -24,7 +24,7 @@ class TestLogInStellarBurgers:
 
     def test_check_button_personal_account(self, driver, page):
         # нажать на кнопку "Личный кабинет" с главной страницы
-        driver.get(settings.URL)
+        driver.get(URLS.URL)
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.MAIN_PAGE_PROFILE_LINK))
         driver.find_element(*page.MAIN_PAGE_PROFILE_LINK).click()
 
@@ -32,7 +32,7 @@ class TestLogInStellarBurgers:
 
     def test_check_button_login_in_form_registration(self, driver, page):
         # нажать на ссылку "вход" в форме регистрации
-        driver.get(settings.URL + "/register")
+        driver.get(URLS.URL + "/register")
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.REG_PAGE_LOGIN_LINK))
         driver.find_element(*page.REG_PAGE_LOGIN_LINK).click()
 
@@ -40,7 +40,7 @@ class TestLogInStellarBurgers:
 
     def test_check_button_in_foggot_password(self, driver, page):
         # нажать на кнопку(ссылку) "вход" в форме восстановления пароля
-        driver.get(settings.URL + "/forgot-password")
+        driver.get(URLS.URL + "/forgot-password")
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(page.RECOVER_PAGE_LINK))
         driver.find_element(*page.RECOVER_PAGE_LINK).click()
 
