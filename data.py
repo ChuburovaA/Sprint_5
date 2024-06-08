dde06e05862f9fbd76afb49d440dd0545454c395
Chuburova Alina <amminami@yandex.ru>
2024-06-08 20:12:16 +0300

    change data.py

--- data.py
+++ data.py
@@ -1,4 +1,11 @@
-class StellarBurgersServiceTestData:
+class LogIn:
     AUTH_EMAIL = "alinachuburova7111@yandex.ru"
     AUTH_PASSWORD = "chuburova7111"
     NAME = "Allina"
+
+class Urls:
+    BASE_URL = 'https://stellarburgers.nomoreparties.site'
+    register = BASE_URL + '/register'
+    forgot = BASE_URL + '/forgot-password'
+    login = BASE_URL + '/login'
+
