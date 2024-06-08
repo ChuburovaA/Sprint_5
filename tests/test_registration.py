52aa8fec708bada0f08053ddffab2f97ecdb8dc6
Chuburova Alina <amminami@yandex.ru>
2024-06-08 20:17:40 +0300

    change test_registration.py in directory tests

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,18 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-from data import StellarBurgersServiceTestData
-from random_emails import generate_random_emails
-from conftest import driver
-from conftest import class_loc
-import URLS
+from data import *
+from conftest import *
+from helpers import generate_random_emails
 
 
+# Проверка регистрации
 class TestStellarBurgersRegistration:
+
     def test_completed_registration(self, driver, class_loc):
-        driver.get(URLS.URL + "/register")
+        driver.get(Urls.register)
         # оказалась на странице с регистрацией
-        driver.find_element(*class_loc.USER_NAME_INPUT).send_keys(StellarBurgersServiceTestData.NAME)
+        driver.find_element(*class_loc.USER_NAME_INPUT).send_keys(LogIn.NAME)
         email = generate_random_emails()
         driver.find_element(*class_loc.USER_EMAIL_INPUT).send_keys(email)
         driver.find_element(*class_loc.USER_PASSWORD_INPUT).send_keys("chuburova123")
@@ -29,9 +29,9 @@ class TestStellarBurgersRegistration:
 
     def test_check_incorrect_password(self, driver, class_loc):
         # проверка ввода некорректного пароля
-        driver.get(URLS.URL + "/register")  # оказалась на странице с регистрацией
+        driver.get(Urls.register)
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(class_loc.REG_BUTTON))
-        driver.find_element(*class_loc.USER_NAME_INPUT).send_keys(StellarBurgersServiceTestData.NAME)
+        driver.find_element(*class_loc.USER_NAME_INPUT).send_keys(LogIn.NAME)
         email = generate_random_emails()
         driver.find_element(*class_loc.USER_EMAIL_INPUT).send_keys(email)
         driver.find_element(*class_loc.USER_PASSWORD_INPUT).send_keys("al")
