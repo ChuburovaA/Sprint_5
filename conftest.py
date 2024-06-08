bf3343d2619bfbe870f83ae9665fe7c73f62c5b4
Chuburova Alina <amminami@yandex.ru>
2024-06-08 20:12:03 +0300

    change conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 import pytest
-import URLS
 from selenium import webdriver
+
+from data import Urls
 from locators import StellarBurgersLocators
 
 @pytest.fixture(scope='function')
 def driver():
     driver = webdriver.Chrome()
-    driver.get(URLS.URL)
+    driver.get(Urls.BASE_URL)
 
     yield driver # возвращает
 
@@ -15,4 +16,7 @@ def driver():
 @pytest.fixture(scope='function')
 def class_loc():
     class_loc = StellarBurgersLocators()
+
     return class_loc
+
+
